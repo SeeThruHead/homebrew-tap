d8dd58eca418728fa135a81bc49369ba4fa326fc
shane keulen <shane.keulen@gmail.com>
2026-04-03 02:33:51 -0400

    Add groovy-cli formula

---
+++ Formula/groovy-cli.rb
@@ -0,0 +1,18 @@
+class GroovyCli < Formula
+  desc "Stream Plex media to MiSTer FPGA CRT via Groovy protocol"
+  homepage "https://github.com/SeeThruHead/groovy-cli"
+  url "https://github.com/SeeThruHead/groovy-cli/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "f7843db520413ea080365516da3e27f08f3175cc94394e30f096a06b254b05e5"
+  license "MIT"
+
+  depends_on "rust" => :build
+  depends_on "homebrew-ffmpeg/ffmpeg/ffmpeg"
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    assert_match "groovy-cli", shell_output("#{bin}/groovy-cli --help")
+  end
+end
